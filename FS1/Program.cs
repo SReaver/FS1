d74e2aab1124cab64d1a72ba5cb53944bcc977f7
плотниковс <@403_4_>
2017-07-13 19:38:54 +0600

    -

--- FS1/Program.cs
+++ FS1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace FS1
 {
@@ -10,6 +11,10 @@ namespace FS1
     {
         static void Main(string[] args)
         {
+            string url = @"C:\Users\плотниковс\Documents\visual studio 2015\Projects\FS1\FS1\text.txt";
+            FileStream fs = new FileStream(url, FileMode.Create);
+            FileStream fs2 = new FileStream(url, FileMode.Create, FileAccess.Write);
+            FileStream fs3 = new FileStream(url, FileMode.Create, FileAccess.Write, FileShare.None);
         }
     }
 }
